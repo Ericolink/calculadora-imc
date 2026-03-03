b227229b3c02fef8dbb2ad0a434fbaac3d188a3d
Eric Munoz <128002939+Ericolink@users.noreply.github.com>
2026-03-03 14:57:27 -0700

    Forzando fallo para demostrar CI

--- tests/test_imc.py
+++ tests/test_imc.py
@@ -5,8 +5,7 @@ from imc import calcular_imc
 def test_imc_correcto():
     """Caso normal"""
     resultado = calcular_imc(70, 1.75)
-    assert round(resultado, 2) == 22.86
-
+    assert round(resultado, 2) == 30
 
 def test_imc_caso_limite():
     """Caso límite con valores pequeños válidos"""
